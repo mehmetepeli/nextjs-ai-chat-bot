c9ddb60a8f9b218265a620244325071717cefaa6
Mehmet Tepeli <mehmettepeli@gmail.com>
2024-02-20 11:49:14 +0100

    updated some things for memory and packages and api route

--- app/api/chat/[chatId]/route.ts
+++ app/api/chat/[chatId]/route.ts
@@ -1,31 +1,32 @@
-import {StreamingTextResponse, LangChainStream} from "ai";
-import {auth,currentUser} from "@clerk/nextjs";
-import {CallbackManager} from "langchain/callbacks";
-import {Replicate} from "langchain/llms/replicate";
-import {NextResponse} from "next/server";
-import {MemoryManager} from "@/lib/memory";
-import {rateLimit} from "@/lib/rate-limit";
+import { StreamingTextResponse, LangChainStream } from "ai";
+import { auth, currentUser } from "@clerk/nextjs";
+import { Replicate } from "langchain/llms/replicate";
+import { CallbackManager } from "langchain/callbacks";
+import { NextResponse } from "next/server";
+
+import { MemoryManager } from "@/lib/memory";
+import { rateLimit } from "@/lib/rate-limit";
 import prismadb from "@/lib/prismadb";
 
-export async function POST(request: Request, {params}: {params: {chatId: string}}) {
+export async function POST(request: Request, { params }: { params: { chatId: string } }) {
     try {
-        const {prompt} = await request.json();
+        const { prompt } = await request.json();
         const user = await currentUser();
 
-        if(!user || !user.firstName || !user.id) {
-            return new NextResponse("Unauthorized", {status: 401});
+        if (!user || !user.firstName || !user.id) {
+            return new NextResponse("Unauthorized", { status: 401 });
         }
 
         const identifier = request.url + "-" + user.id;
-        const {success} = await rateLimit(identifier);
+        const { success } = await rateLimit(identifier);
 
-        if(!success) {
-            return new NextResponse("Rate limit exceeded", {status: 429});
+        if (!success) {
+            return new NextResponse("Rate limit exceeded", { status: 429 });
         }
 
         const character = await prismadb.character.update({
             where: {
-                id: params.chatId,
+                id: params.chatId
             },
             data: {
                 messages: {
@@ -33,58 +34,66 @@ export async function POST(request: Request, {params}: {params: {chatId: string}
                         content: prompt,
                         role: "user",
                         userId: user.id,
-                    }
-                }
+                    },
+                },
             }
         });
 
-        if(!character) {
-            return new NextResponse("Character not found", {status: 404});
+        if (!character) {
+            return new NextResponse("Character not found", { status: 404 });
         }
 
         const name = character.id;
         const character_file_name = name + ".txt";
 
         const characterKey = {
-            characterName: name,
+            characterName: name!,
             userId: user.id,
-            modelName: "llama2-13b"
+            modelName: "llama2-13b",
         };
-
         const memoryManager = await MemoryManager.getInstance();
-        const records = await memoryManager.readLatestHistory(characterKey);
 
-        if(records.length === 0) {
+        const records = await memoryManager.readLatestHistory(characterKey);
+        if (records.length === 0) {
             await memoryManager.seedChatHistory(character.seed, "\n\n", characterKey);
         }
-
         await memoryManager.writeToHistory("User: " + prompt + "\n", characterKey);
+
+        // Query Pinecone
 
         const recentChatHistory = await memoryManager.readLatestHistory(characterKey);
 
-        const similarDocs = await memoryManager.vectorSearch(recentChatHistory, character_file_name,);
+        // Right now the preamble is included in the similarity search, but that
+        // shouldn't be an issue
+
+        const similarDocs = await memoryManager.vectorSearch(
+            recentChatHistory,
+            character_file_name
+        );
 
         let relevantHistory = "";
-
-        if(!!similarDocs && similarDocs.length !== 0) {
+        if (!!similarDocs && similarDocs.length !== 0) {
             relevantHistory = similarDocs.map((doc) => doc.pageContent).join("\n");
         }
-
-        const {handlers} = LangChainStream();
-
+        const { handlers } = LangChainStream();
+        // Call Replicate for inference
         const model = new Replicate({
-            model: "a16z-infra/llama-2-13b-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
+            model:
+                "a16z-infra/llama-2-13b-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
             input: {
-                max_length: 2048
+                max_length: 2048,
             },
             apiKey: process.env.REPLICATE_API_TOKEN,
             callbackManager: CallbackManager.fromHandlers(handlers),
         });
 
+        // Turn verbose on for debugging
         model.verbose = true;
 
-        const resp = String(await model
-            .call(`
+        const resp = String(
+            await model
+                .call(
+                    `
         ONLY generate plain sentences without prefix of who is speaking. DO NOT use ${character.name}: prefix. 
 
         ${character.instructions}
@@ -92,7 +101,11 @@ export async function POST(request: Request, {params}: {params: {chatId: string}
         Below are relevant details about ${character.name}'s past and the conversation you are in.
         ${relevantHistory}
 
-        ${recentChatHistory}\n${character.name}:`).catch(console.error));
+
+        ${recentChatHistory}\n${character.name}:`
+                )
+                .catch(console.error)
+        );
 
         const cleaned = resp.replaceAll(",", "");
         const chunks = cleaned.split("\n");
@@ -104,30 +117,27 @@ export async function POST(request: Request, {params}: {params: {chatId: string}
         let s = new Readable();
         s.push(response);
         s.push(null);
-
-        if(response !== undefined && response.length > 1) {
+        if (response !== undefined && response.length > 1) {
             memoryManager.writeToHistory("" + response.trim(), characterKey);
 
             await prismadb.character.update({
                 where: {
-                    id: params.chatId,
+                    id: params.chatId
                 },
                 data: {
                     messages: {
                         create: {
                             content: response.trim(),
                             role: "system",
-                            userId: user.id
-                        }
-                    }
+                            userId: user.id,
+                        },
+                    },
                 }
             });
         }
 
         return new StreamingTextResponse(s);
-
-    } catch (e) {
-        console.log("[CHAT_POST]", e);
-        return new NextResponse("Internal Error", {status: 500});
+    } catch (error) {
+        return new NextResponse("Internal Error", { status: 500 });
     }
-}
+};
--- lib/memory.ts
+++ lib/memory.ts
@@ -7,7 +7,7 @@ export type CharacterKey = {
     characterName: string;
     modelName: string;
     userId: string;
-}
+};
 
 export class MemoryManager {
     private static instance: MemoryManager;
@@ -20,53 +20,56 @@ export class MemoryManager {
     }
 
     public async init() {
-        if(this.vectorDBClient instanceof PineconeClient) {
+        if (this.vectorDBClient instanceof PineconeClient) {
             await this.vectorDBClient.init({
                 apiKey: process.env.PINECONE_API_KEY!,
-                environment: process.env.PINECONE_ENVIRONMENT!
+                environment: process.env.PINECONE_ENVIRONMENT!,
             });
         }
     }
 
-    public async vectorSearch(recentChatHistory: string, characterFileName: string) {
+    public async vectorSearch(
+        recentChatHistory: string,
+        characterFileName: string
+    ) {
         const pineconeClient = <PineconeClient>this.vectorDBClient;
 
-        const pineconeIndex = pineconeClient.index(process.env.PINECONE_INDEX! || "");
+        const pineconeIndex = pineconeClient.Index(
+            process.env.PINECONE_INDEX! || ""
+        );
 
         const vectorStore = await PineconeStore.fromExistingIndex(
-            new OpenAIEmbeddings({openAIApiKey: process.env.OPENAI_API_KEY}),
-            {pineconeIndex}
+            new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY }),
+            { pineconeIndex }
         );
 
         const similarDocs = await vectorStore
-            .similaritySearch(recentChatHistory, 3, {fileName: characterFileName})
+            .similaritySearch(recentChatHistory, 3, { fileName: characterFileName })
             .catch((err) => {
-            console.log("Failed to get vector search results", err);
+                console.log("WARNING: failed to get vector search results.", err);
             });
-
         return similarDocs;
     }
 
     public static async getInstance(): Promise<MemoryManager> {
-        if(!MemoryManager.instance) {
+        if (!MemoryManager.instance) {
             MemoryManager.instance = new MemoryManager();
             await MemoryManager.instance.init();
         }
-
         return MemoryManager.instance;
     }
 
-    private generateRedisCharacterKey(characterKey: CharacterKey): string {
-        return `${characterKey.characterName}-${characterKey.modelName}-${characterKey.userId}`;
+    private generateRedisCompanionKey(characterKey: CharacterKey): string {
+        return `${characterKey.companionName}-${characterKey.modelName}-${characterKey.userId}`;
     }
 
     public async writeToHistory(text: string, characterKey: CharacterKey) {
-        if(!characterKey || typeof  characterKey.userId == "undefined") {
-            console.log("Character Key set incorrectly");
+        if (!companionKey || typeof companionKey.userId == "undefined") {
+            console.log("Character key set incorrectly");
             return "";
         }
 
-        const key = this.generateRedisCharacterKey(characterKey);
+        const key = this.generateRedisCompanionKey(companionKey);
         const result = await this.history.zadd(key, {
             score: Date.now(),
             member: text,
@@ -76,36 +79,38 @@ export class MemoryManager {
     }
 
     public async readLatestHistory(characterKey: CharacterKey): Promise<string> {
-        if(!characterKey || typeof characterKey.userId == "undefined") {
-            console.log("Character Key set incorrectly");
+        if (!characterKey || typeof characterKey.userId == "undefined") {
+            console.log("Character key set incorrectly");
             return "";
         }
 
-        const key = this.generateRedisCharacterKey(characterKey);
+        const key = this.generateRedisCompanionKey(characterKey);
         let result = await this.history.zrange(key, 0, Date.now(), {
             byScore: true,
         });
 
         result = result.slice(-30).reverse();
         const recentChats = result.reverse().join("\n");
-
         return recentChats;
     }
 
-    public async seedChatHistory(seedContent: String, delimiter: string = "\n", characterKey: CharacterKey) {
-        const key = this.generateRedisCharacterKey(characterKey);
-
-        if(await this.history.exists(key)) {
-            console.log("User already has chat history")
+    public async seedChatHistory(
+        seedContent: String,
+        delimiter: string = "\n",
+        characterKey: CharacterKey
+    ) {
+        const key = this.generateRedisCompanionKey(characterKey);
+        if (await this.history.exists(key)) {
+            console.log("User already has chat history");
             return;
         }
 
         const content = seedContent.split(delimiter);
         let counter = 0;
-
-        for(const line of content) {
-            await this.history.zadd(key, {score: counter, member: line});
+        for (const line of content) {
+            await this.history.zadd(key, { score: counter, member: line });
             counter += 1;
         }
     }
 }
+
